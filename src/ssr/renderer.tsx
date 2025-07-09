ecbbd36fa8ccc1ebf3cdbce2b0ec5fa9d8de7984
Fasih Mehmood <fasih.mehmood@canonical.com>
2025-07-09 12:09:43 +0500

    fix: read file at runtime

--- src/ssr/renderer.tsx
+++ src/ssr/renderer.tsx
@@ -1,6 +1,16 @@
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
 import { JSXRenderer } from "@canonical/react-ssr/renderer";
-import htmlString from "../../dist/client/index.html?raw";
 import EntryServer from "./entry-server.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const htmlString = readFileSync(
+  path.resolve(__dirname, "../../dist/client/index.html"),
+  "utf8"
+);
 
 const Renderer = new JSXRenderer(EntryServer, {
   htmlString,
